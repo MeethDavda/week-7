82faa16f6c15fb5fc2ca574278aee656001edd23
MeethDavda <mjdavda02@gmail.com>
2023-08-18 14:13:29 +0530

    completed week-7

--- admin-client/src/App.jsx
+++ admin-client/src/App.jsx
@@ -1,33 +1,78 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signin from "./components/Signin.jsx";
 import Signup from "./components/Signup.jsx";
 import Appbar from "./components/Appbar.jsx";
 import AddCourse from "./components/AddCourse.jsx";
 import Courses from "./components/Courses";
 import Course from "./components/Course";
-import {Landing} from "./components/Landing.jsx";
+import { Landing } from "./components/Landing.jsx";
+import { userState } from "./store/atoms/user.js";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useSetRecoilState,
+  useRecoilValue,
+} from "recoil";
+import axios from "axios";
+import { BASE_URL } from "./config.js";
+import { useEffect } from "react";
 
 function App() {
+  return (
+    <div
+      style={{ width: "100vw", height: "100vh", backgroundColor: "#eeeeee" }}
+    >
+      <RecoilRoot>
+        <Router>
+          <Appbar />
+          <InitUser />
+          <Routes>
+            <Route path={"/addcourse"} element={<AddCourse />} />
+            <Route path={"/course/:courseId"} element={<Course />} />
+            <Route path={"/courses"} element={<Courses />} />
+            <Route path={"/signin"} element={<Signin />} />
+            <Route path={"/signup"} element={<Signup />} />
+            <Route path={"/"} element={<Landing />} />
+          </Routes>
+        </Router>
+      </RecoilRoot>
+    </div>
+  );
+}
 
-    return (
-        <div style={{width: "100vw",
-            height: "100vh",
-            backgroundColor: "#eeeeee"}}
-        >
-                <Router>
-                    <Appbar />
-                    <Routes>
-                        <Route path={"/addcourse"} element={<AddCourse />} />
-                        <Route path={"/course/:courseId"} element={<Course />} />
-                        <Route path={"/courses"} element={<Courses />} />
-                        <Route path={"/signin"} element={<Signin />} />
-                        <Route path={"/signup"} element={<Signup />} />
-                        <Route path={"/"} element={<Landing />} />
-                    </Routes>
-                </Router>
-
-        </div>
-    );
+function InitUser() {
+  const setUser = useSetRecoilState(userState);
+  const init = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/admin/me`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+      if (response.data.username) {
+        setUser({
+          isLoading: false,
+          userEmail: response.data.username,
+        });
+      } else {
+        setUser({
+          isLoading: false,
+          userEmail: null,
+        });
+      }
+    } catch (error) {
+      setUser({
+        isLoading: false,
+        userEmail: null,
+      });
+    }
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return <></>;
 }
 
 export default App;
--- admin-client/src/components/Appbar.jsx
+++ admin-client/src/components/Appbar.jsx
@@ -1,104 +1,126 @@
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { useEffect, useState } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../config.js";
 import axios from "axios";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { userEmailState } from "../store/selectors/userEmail.js";
+import { isUserLoading } from "../store/selectors/isUserLoading.js";
+import { userState } from "../store/atoms/user.js";
 
 function Appbar() {
-    const navigate = useNavigate()
-    const [userEmail, setUserEmail] = useState(null);
+  const navigate = useNavigate();
+  const userEmail = useRecoilValue(userEmailState);
+  const userLoading = useRecoilValue(isUserLoading);
+  const setUser = useSetRecoilState(userState);
 
-    const init = async() => {
-        const response = await axios.get(`${BASE_URL}/admin/me`, {
-            headers: {
-                "Authorization": "Bearer " + localStorage.getItem("token")
-            }
-        })
+  if (userLoading) {
+    return <>Loading..</>;
+  }
 
-        if (response.data.username) {
-            setUserEmail(response.data.username)
-        }
-    };
-
-    useEffect(() => {
-       init();
-    }, []);
-
-    if (userEmail) {
-        return <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: 4,
-            zIndex: 1
-        }}>
-            <div style={{marginLeft: 10, cursor: "pointer"}} onClick={() => {
-                navigate("/")
-            }}>
-                <Typography variant={"h6"}>Coursera</Typography>
-            </div>
-    
-            <div style={{display: "flex"}}>
-                <div style={{marginRight: 10, display: "flex"}}>
-                <div style={{marginRight: 10}}>
-                        <Button
-                            onClick={() => {
-                                navigate("/addcourse")
-                            }}
-                        >Add course</Button>
-                    </div>
-
-                    <div style={{marginRight: 10}}>
-                        <Button
-                            onClick={() => {
-                                navigate("/courses")
-                            }}
-                        >Courses</Button>
-                    </div>
-
-                    <Button
-                        variant={"contained"}
-                        onClick={() => {
-                            localStorage.setItem("token", null);
-                            window.location = "/";
-                        }}
-                    >Logout</Button>
-                </div>
-            </div>
+  if (userEmail) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 4,
+          zIndex: 1,
+        }}
+      >
+        <div
+          style={{ marginLeft: 10, cursor: "pointer" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <Typography variant={"h6"}>Coursera</Typography>
         </div>
-    } else {
-        return <div style={{
-            display: "flex",
-            justifyContent: "space-between",
-            padding: 4,
-            zIndex: 1
-        }}>
-            <div style={{marginLeft: 10, cursor: "pointer"}} onClick={() => {
-                navigate("/")
-            }}>
-                <Typography variant={"h6"}>Coursera</Typography>
+
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: 10, display: "flex" }}>
+            <div style={{ marginRight: 10 }}>
+              <Button
+                onClick={() => {
+                  navigate("/addcourse");
+                }}
+              >
+                Add course
+              </Button>
             </div>
-    
-            <div style={{display: "flex"}}>
-                <div style={{marginRight: 10}}>
-                    <Button
-                        variant={"contained"}
-                        onClick={() => {
-                            navigate("/signup")
-                        }}
-                    >Signup</Button>
-                </div>
-                <div>
-                    <Button
-                        variant={"contained"}
-                        onClick={() => {
-                            navigate("/signin")
-                        }}
-                    >Signin</Button>
-                </div>
+
+            <div style={{ marginRight: 10 }}>
+              <Button
+                onClick={() => {
+                  navigate("/courses");
+                }}
+              >
+                Courses
+              </Button>
             </div>
+
+            <Button
+              variant={"contained"}
+              onClick={() => {
+                localStorage.setItem("token", null);
+                // window.location = "/";
+                setUser({
+                  isLoading: false,
+                  userEmail: null,
+                });
+              }}
+            >
+              Logout
+            </Button>
+          </div>
         </div>
-    }
+      </div>
+    );
+  } else {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 4,
+          zIndex: 1,
+        }}
+      >
+        <div
+          style={{ marginLeft: 10, cursor: "pointer" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <Typography variant={"h6"}>Coursera</Typography>
+        </div>
+
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: 10 }}>
+            <Button
+              variant={"contained"}
+              onClick={() => {
+                navigate("/signup");
+              }}
+            >
+              Signup
+            </Button>
+          </div>
+          <div>
+            <Button
+              variant={"contained"}
+              onClick={() => {
+                navigate("/signin");
+              }}
+            >
+              Signin
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Appbar;
